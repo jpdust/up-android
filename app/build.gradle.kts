726e760ba4d96a7216d40dc644ef4920737ba786
John Dust <jpdust@gmail.com>
2026-04-28 08:57:38 -0500

    fixing scroll issue where countries disappear while zoomed in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.unstampedpages.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20260427
+        versionCode = 202604271
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
